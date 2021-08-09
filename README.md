691c76c56742072d5e287fd23be31b0da7645145
b_bado <bado.bernard94@gmail.com>
2021-08-09 13:48:41 +0200

    adding readme

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # medium-friendly-lamp
+
+## How To Start
+
+Install dependencies.
+
+```
+npm install
+```
+
+Open `index.js` and paste the link to any medium profile into `getFollowers` function
+
+Run
+
+```
+npm start
+```
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const getFollowers = require("./functions/getFollowers");
 
-getFollowers("https://bernardbad.medium.com");
+// Paste link to medium profile here
+// e.g https://bernardbad.medium.com
+getFollowers("Paste Your Medium Profile Link Here");
